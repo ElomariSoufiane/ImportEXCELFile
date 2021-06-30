c31ce1bca6d7e86918edf8f4d2408e54f836c5de
Soufiane Elomari <SoufianeElomari@fmc-ag.com>
2021-06-30 12:27:05 +0200

    Ajoutez des fichiers projet.

---
+++ ImportExcel/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace ImportExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DgvAjout = new System.Windows.Forms.DataGridView();
+            this.btnCharger = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvAjout)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DgvAjout
+            // 
+            this.DgvAjout.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvAjout.Location = new System.Drawing.Point(12, 12);
+            this.DgvAjout.Name = "DgvAjout";
+            this.DgvAjout.Size = new System.Drawing.Size(1392, 578);
+            this.DgvAjout.TabIndex = 0;
+            // 
+            // btnCharger
+            // 
+            this.btnCharger.Location = new System.Drawing.Point(1270, 610);
+            this.btnCharger.Name = "btnCharger";
+            this.btnCharger.Size = new System.Drawing.Size(134, 32);
+            this.btnCharger.TabIndex = 1;
+            this.btnCharger.Text = "Charger";
+            this.btnCharger.UseVisualStyleBackColor = true;
+            this.btnCharger.Click += new System.EventHandler(this.btnCharger_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1370, 676);
+            this.Controls.Add(this.btnCharger);
+            this.Controls.Add(this.DgvAjout);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvAjout)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DgvAjout;
+        private System.Windows.Forms.Button btnCharger;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ ImportExcel/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace ImportExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private string ExcelConnection(string fileName)
+        {
+            return @"Provider=Microsoft.ACE.OLEDB.12.0;" +
+                   @"Data Source=" + fileName + ";" +
+                   @"Extended Properties=" + Convert.ToChar(34).ToString() +
+                   @"Excel 12.0 xml" + Convert.ToChar(34).ToString() + ";";
+        }
+        private void btnCharger_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string Fichier = openFileDialog1.FileName;
+            DataSet dat = default(DataSet);
+            dat = new DataSet();
+
+            using (OleDbConnection Conn =
+                new OleDbConnection(ExcelConnection(Fichier))) 
+            {
+                Conn.Open();
+                //délaration du DataAdapter
+                //notre requete sélectionne toute les cellules de la feuille spécifié
+
+                using (OleDbDataAdapter adap = new OleDbDataAdapter("select * from [Transport$]", Conn))
+                {
+                    adap.TableMappings.Add("Table", "TestTable");
+                    //Chargement du Dataset
+                    adap.Fill(dat);
+                    //On Charge les données sur le DGV
+                    DgvAjout.DataSource = dat.Tables[0];
+                }
+                //libèrer les ressources 
+                Conn.Close();
+            }
+        }
+    }
+}
+    
+
